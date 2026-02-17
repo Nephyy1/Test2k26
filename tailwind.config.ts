3330cd56454b2a15ea88f1889d358f1ea6baba36
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2026-02-17 18:31:54 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,9 +26,6 @@ const config: Config = {
         "neo-sm": "2px 2px 0px 0px rgba(0,0,0,1)",
         "neo-hover": "2px 2px 0px 0px rgba(0,0,0,1)",
       },
-      fontFamily: {
-        sans: ['var(--font-space-grotesk)', 'sans-serif'],
-      }
     },
   },
   plugins: [],
